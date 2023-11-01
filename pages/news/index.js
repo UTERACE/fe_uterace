46029a6a4279134a53563e91fa51e788b2270708
doanquocbao <doanquocbao4117@gmail.com>
2023-11-01 16:42:05 +0700

    update api news

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Paginator } from 'primereact/paginator'
 import DataView from '@/components/dataview/DataView'
 import Title from '@/components/landing/Title'
@@ -6,74 +6,94 @@ import Link from 'next/link'
 import LocaleHelper from '@/components/locale/LocaleHelper'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import apiInstance from '@/api/apiInstance'
 
 const NewsPage = () => {
   const [news, setNews] = useState([])
   const [current_page, setCurrentPage] = useState(1)
-  const [per_page, setPerPage] = useState(5)
+  const [per_page, setPerPage] = useState(6)
   const [totalRecords, setTotalRecords] = useState(1)
   const [first, setFirst] = useState(0)
   const { t } = useTranslation('news')
-  const data = {
-    per_page: 5,
-    current_page: 1,
-    total_page: 5,
-    total_news: 22,
-    news: [
-      {
-        news_id: 1,
-        name: 'Hướng dẫn đăng ký và cài đặt ứng dụng Strava',
-        description:
-          'Hướng dẫn chi tiết cách đăng ký tài khoản và cài đặt ứng dụng Strava trên điện thoại',
-        image: 'https://mobirace.net/Upload/Images/HuongDan/dang_ky_strava.png',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-      {
-        news_id: 2,
-        name: 'Hướng dẫn ghi nhận thành tích chạy trong nhà, trên máy',
-        description:
-          'Hướng dẫn chạy ở chế độ indoor trong nhà/trên máy chạy bộ',
-        image: 'https://mobirace.net/Upload/Images/HuongDan/run_indoor.jpg',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-      {
-        news_id: 3,
-        name: 'Hướng dẫn tham gia giải chạy tại UTE Marathon 2021',
-        description: 'Hướng dẫn tham gia giải chạy tại UTE Marathon 2021',
-        image: 'https://mobirace.net/Upload/Images/HuongDan/dangkygiai.png',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-      {
-        news_id: 4,
-        name: 'Hướng dẫn kết nối ứng dụng Strava với Mobirace',
-        description:
-          'Hướng dẫn kết nối ứng dụng Strava với Mobirace để đồng bộ dữ liệu chạy bộ từ Strava về Mobirace',
-        image: 'https://mobirace.net/Upload/Images/HuongDan/ketnoi_strava.png',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-      {
-        news_id: 5,
-        name: 'Hướng dẫn đăng ký và cài đặt ứng dụng Strava',
-        description:
-          'Hướng dẫn chi tiết cách đăng ký tài khoản và cài đặt ứng dụng Strava trên điện thoại',
-        image: 'https://picsum.photos/200/300',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-      {
-        news_id: 6,
-        name: 'Các bước tham gia giải chạy bộ tại Mobirace',
-        description:
-          'Hướng dẫn chi tiết các bước tham gia giải chạy bộ tại Mobirace',
-        image: 'https://picsum.photos/200/300',
-        createAt: '2023-08-01T00:00:00Z',
-        updateAt: '2023-09-01T00:00:00Z',
-      },
-    ],
+  // const data = {
+  //   per_page: 5,
+  //   current_page: 1,
+  //   total_page: 5,
+  //   total_news: 22,
+  //   news: [
+  //     {
+  //       news_id: 1,
+  //       name: 'Hướng dẫn đăng ký và cài đặt ứng dụng Strava',
+  //       description:
+  //         'Hướng dẫn chi tiết cách đăng ký tài khoản và cài đặt ứng dụng Strava trên điện thoại',
+  //       image: 'https://mobirace.net/Upload/Images/HuongDan/dang_ky_strava.png',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //     {
+  //       news_id: 2,
+  //       name: 'Hướng dẫn ghi nhận thành tích chạy trong nhà, trên máy',
+  //       description:
+  //         'Hướng dẫn chạy ở chế độ indoor trong nhà/trên máy chạy bộ',
+  //       image: 'https://mobirace.net/Upload/Images/HuongDan/run_indoor.jpg',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //     {
+  //       news_id: 3,
+  //       name: 'Hướng dẫn tham gia giải chạy tại UTE Marathon 2021',
+  //       description: 'Hướng dẫn tham gia giải chạy tại UTE Marathon 2021',
+  //       image: 'https://mobirace.net/Upload/Images/HuongDan/dangkygiai.png',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //     {
+  //       news_id: 4,
+  //       name: 'Hướng dẫn kết nối ứng dụng Strava với Mobirace',
+  //       description:
+  //         'Hướng dẫn kết nối ứng dụng Strava với Mobirace để đồng bộ dữ liệu chạy bộ từ Strava về Mobirace',
+  //       image: 'https://mobirace.net/Upload/Images/HuongDan/ketnoi_strava.png',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //     {
+  //       news_id: 5,
+  //       name: 'Hướng dẫn đăng ký và cài đặt ứng dụng Strava',
+  //       description:
+  //         'Hướng dẫn chi tiết cách đăng ký tài khoản và cài đặt ứng dụng Strava trên điện thoại',
+  //       image: 'https://picsum.photos/200/300',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //     {
+  //       news_id: 6,
+  //       name: 'Các bước tham gia giải chạy bộ tại Mobirace',
+  //       description:
+  //         'Hướng dẫn chi tiết các bước tham gia giải chạy bộ tại Mobirace',
+  //       image: 'https://picsum.photos/200/300',
+  //       createAt: '2023-08-01T00:00:00Z',
+  //       updateAt: '2023-09-01T00:00:00Z',
+  //     },
+  //   ],
+  // }
+  useEffect(() => {
+    fetchNews()
+  }, [])
+  const fetchNews = async () => {
+    try {
+      const res = await apiInstance.get(
+        `/news?current_page=${current_page}&per_page=${per_page}`
+      )
+      const data = res.data
+      if (res.status === 200) {
+        setNews(data.news)
+        setTotalRecords(data.total_news)
+        setCurrentPage(data.current_page)
+        setPerPage(data.per_page)
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
   const itemTemplate = (item) => {
     return (
@@ -88,11 +108,11 @@ const NewsPage = () => {
           <div id='info-dataview'>
             <h4>
               {t('created-at')}:{' '}
-              {LocaleHelper.formatDateTime(new Date(item.createAt))}
+              {LocaleHelper.formatDateTime(new Date(item.created_at))}
             </h4>
             <h4>
               {t('updated-at')}:{' '}
-              {LocaleHelper.formatDateTime(new Date(item.updateAt))}
+              {LocaleHelper.formatDateTime(new Date(item.updated_at))}
             </h4>
           </div>
           <div id='name-dataview'>
@@ -109,7 +129,8 @@ const NewsPage = () => {
                   <i className='pi pi-arrow-right' aria-hidden='true'></i>
                 </Link>
                 <Link id='link-dataview' href='/share'>
-                  {t('share')} <i className='pi pi-share-alt' aria-hidden='true'></i>
+                  {t('share')}{' '}
+                  <i className='pi pi-share-alt' aria-hidden='true'></i>
                 </Link>
               </div>
             </div>
@@ -125,19 +146,19 @@ const NewsPage = () => {
   }
   return (
     <div className='centered-content-dataview'>
-      <Title title={t('news')}/>
+      <Title title={t('news')} />
       <DataView
-        data={data.news}
+        data={news}
         href='/news/news-detail/'
         itemTemplate={itemTemplate}
       />
       <Paginator
         first={first}
-        rows={data.per_page}
-        totalRecords={data.total_news}
-        rowsPerPageOptions={[5, 10, 15]}
+        rows={per_page}
+        totalRecords={totalRecords}
+        rowsPerPageOptions={[6, 9, 12]}
         onPageChange={onPageChange}
-        page={data.current_page}
+        page={current_page}
       />
     </div>
   )
@@ -147,7 +168,7 @@ export default NewsPage
 export const getStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['news','topbar'])),
+      ...(await serverSideTranslations(locale, ['news', 'topbar'])),
     },
   }
 }
