e6a0758a3128455f9c5b5e75df41f5476b0fb080
doanquocbao <doanquocbao4117@gmail.com>
2024-06-28 22:14:00 +0700

    update locale

--- components/locale/LocaleHelper.js
+++ components/locale/LocaleHelper.js
@@ -16,13 +16,23 @@ function formatDate(date, locale = defaultLocale) {
 }
 
 function formatDateTime(date, locale = defaultLocale) {
-  if (date instanceof Date && !isNaN(date.getTime())) {
-    return new Intl.DateTimeFormat(locale, {
-      dateStyle: 'medium',
-      timeStyle: 'medium',
-    }).format(date)
+  if (typeof date === 'string') {
+    date = new Date(date);
   }
-  return date
+
+  if (date instanceof Date && !isNaN(date.getTime())) {
+    return date.toLocaleString(locale, {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour12: false,
+      timeZone: 'UTC'
+    }).replace(',', '');
+  }
+  return date;
 }
 function formatDateComment(date, locale = defaultLocale) {
   // 2024-04-21 20:02:42.774
--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://be-uterace.onrender.com/api'
 // export const API_URL = 'http://localhost:8080/api'
-// export const API_URL = 'http://192.168.1.3:8080/api'
+// export const API_URL = 'https://be-uterace-ltf2.onrender.com/api'
 export const API_TIMEOUT = 240000
 export const API_HEADERS = {
   'Content-Type': 'application/json',
